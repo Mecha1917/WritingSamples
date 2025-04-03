3427282d768b5fa8cf3129a6037ae04c80d32db7
Mecha1917 <123705550+Mecha1917@users.noreply.github.com>
2025-04-03 00:00:20 -0700

    Adjusted logarithm calculator to now incorporate the functionality to calculate Base-10 Logarithm or do Natural Logarithm.
    Value Add:
    Users now can better utilise the features of the program to now calculate the natural log OR base-10 log of a specific number and choose which one to use. 
    
    Use Cases:
    Users can better use this better calculate the natural log of a number and be able to calculate the base-10 for the same number if ran again.
    
    Rationale for Choices:
    When adding onto the code, I found it more viable and simplier to combine the function area for the logarithm calculations to incorporate both the Natural Log and Base-10 Log, to both compact the design for easier compiling, as well as simplifying the process.

--- Test.f95
+++ Test.f95
@@ -1,6 +1,7 @@
-program first_func
+program first_func 
   implicit none
-  real :: base, exponent, result, num, num2, log_result
+  real :: base, exponent, result, num2, log_result
+  integer :: log_choice
 
   ! Prompt the user for base and exponent input
   print *, 'Enter a base number:'
@@ -15,15 +16,23 @@ program first_func
   print *, 'The number raised to the given power is:', result
 
   ! Prompt the user for another input
-  print *, 'Enter another number to compute its natural logarithm:'
+  print *, 'Enter another number to compute its logarithm:'
   read *, num2
 
-  ! Compute the natural logarithm
   if (num2 > 0.0) then
-    log_result = log(num2)
-    print *, 'The natural log of the number is:', log_result
+    print *, 'Choose logarithm type: 1 for Natural Log (ln), 2 for Base 10 Log (log10)'
+    read *, log_choice
+    
+    if (log_choice == 1) then
+      log_result = log(num2)
+      print *, 'The natural log of the number is:', log_result
+    else if (log_choice == 2) then
+      log_result = log10(num2)
+      print *, 'The base 10 log of the number is:', log_result
+    else
+      print *, 'Error: Invalid, Please Enter 1 or 2.'
+    end if
   else
     print *, 'Error: Logarithm is undefined for non-positive values.'
   end if
-
 end program first_func
