facd3cab9881eb018469bd28da7c85880acd79f2
Mecha1917 <123705550+Mecha1917@users.noreply.github.com>
2025-04-04 23:20:03 -0700

    Made a heavy vision of the calculator to change the overall functionality to where now you can either use power or log in order to reduce complexity and improve runtime.
    Value Add:
    Users can now have a much easier time running the program to either just use power functions or log functions to find their answer for various numbers and inputs. 
    
    Use Cases:
    Teachers using this program can now have a easier time running the program to calculate either the power of a number or find the log of a number and whether to use Natural Log or Base-10 Log.
    
    Rationale for Choices:
    When adjusting and rewriting the code, I choose to do this in order to improve the running capability of the program and no longer force people to calculate a power number and be forced to calculate a log number. This was done to give users far more control over the program to find their necessary solutions and quickly be able to get their answer without sitting through a long process.

--- Test.f95
+++ Test.f95
@@ -1,38 +1,46 @@
-program first_func 
+program first_func
   implicit none
   real :: base, exponent, result, num2, log_result
-  integer :: log_choice
+  integer :: choice, log_choice
 
-  ! Prompt the user for base and exponent input
-  print *, 'Enter a base number:'
-  read *, base
-  print *, 'Enter an exponent:'
-  read *, exponent
+  print *, 'Choose which math operation to use:'
+  print *, '1. Power Calculator'
+  print *, '2. Logarithm Calculator'
+  read *, choice
 
-  ! Compute the power
-  result = base ** exponent
+    ! Power Calculator
+  if (choice == 1) then
+    print *, 'Enter a base number:'
+    read *, base
+    print *, 'Enter an exponent:'
+    read *, exponent
 
-  ! Display the result
-  print *, 'The number raised to the given power is:', result
-
-  ! Prompt the user for another input
-  print *, 'Enter another number to compute its logarithm:'
-  read *, num2
-
-  if (num2 > 0.0) then
-    print *, 'Choose logarithm type: 1 for Natural Log (ln), 2 for Base 10 Log (log10)'
-    read *, log_choice
+    result = base ** exponent
+    print *, 'The number raised to the inputted power is:', result
     
-    if (log_choice == 1) then
-      log_result = log(num2)
-      print *, 'The natural log of the number is:', log_result
-    else if (log_choice == 2) then
-      log_result = log10(num2)
-      print *, 'The base 10 log of the number is:', log_result
+    ! Logarithm Types Calculator
+  else if (choice == 2) then
+    print *, 'Enter a number to compute its logarithm:'
+    read *, num2
+
+    if (num2 > 0.0) then
+      print *, 'Choose logarithm type: 1 for Natural Log (ln), 2 for Base 10 Log (log10)'
+      read *, log_choice
+
+      if (log_choice == 1) then
+        log_result = log(num2)
+        print *, 'The natural log of the number is:', log_result
+      else if (log_choice == 2) then
+        log_result = log10(num2)
+        print *, 'The base 10 log of the number is:', log_result
+      else
+        print *, 'Error: Invalid choice. Please enter 1 or 2.'
+      end if
     else
-      print *, 'Error: Invalid, Please Enter 1 or 2.'
+      print *, 'Error: Logarithm is undefined for non-positive values.'
     end if
+
   else
-    print *, 'Error: Logarithm is undefined for non-positive values.'
+    print *, 'Error: Invalid choice. Please enter 1 or 2.'
   end if
 end program first_func
